ad1096159275154fbb4b05e099ad878c4553c33e
koukita <kou35tochas@gmail.com>
2015-07-26 20:39:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ L.tileLayer('http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
 attribution: "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>"
 }).addTo(map);
 
-map.setView([35.681382, 139.766084], 15);
+map.setView([42.827639, 140.811768], 11);
