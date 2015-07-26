f7c260af3b7947147caff36bc64535ca947ec28c
koukita <kou35tochas@gmail.com>
2015-07-26 20:40:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ L.tileLayer('http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
 attribution: "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>"
 }).addTo(map);
 
+//羊蹄山を中心に表示
 map.setView([42.827639, 140.811768], 11);
